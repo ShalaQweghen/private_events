f54b5f85e18853582e208c24bafe4dfe1c43fa12
Shala Qweghen <serafettinyilmaz04@gmail.com>
2016-08-31 10:38:58 +0300

    Fix invitations

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -7,7 +7,7 @@ class InvitationsController < ApplicationController
 
   def create
   	@invitation = Invitation.new
-  	@invitation.attended_event_id = Event.find_by(name: params[:invitation][:attended_event_id]).id
+  	@invitation.attended_event_id = params[:invitation][:attended_event_id]
   	@invitation.attendee_id = User.find_by(name: params[:invitation][:attendee_id]).id
   	if @invitation.save
   		redirect_back(fallback_location: @invitation.attended_event_id)
